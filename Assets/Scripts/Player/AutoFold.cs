0e7aa5680c343f16953e7458c91cc081059bc9da
Miroslav Beka <mirobeka@gmail.com>
2021-03-22 19:05:58 +0100

    trošku vylepšena scena, steny sa skladaju

---
+++ Assets/Scripts/Player/AutoFold.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoFold : MonoBehaviour {
+
+    public float fadeRate = 0.5f;
+
+    private GameObject normalObject;
+    private GameObject foldedObject;
+    private float fade;
+    private bool shouldBeFolded = false;
+
+    public void BeFolded() {
+        Fold();
+        shouldBeFolded = true;
+        fade = 0f;
+    }
+
+    private void Start() {
+        normalObject = transform.Find("FullSize").gameObject;
+        Transform foldedTransform = transform.Find("CutSize");
+        if(foldedTransform != null){
+            foldedObject = foldedTransform.gameObject;
+            Debug.Log("found folded version");
+        }
+
+        CopyColiderFromChild();
+
+        // unfold by default
+        UnFold();
+    }
+
+    // Update is called once per frame
+    private void Update() {
+        if (fade >= 1 && !shouldBeFolded){
+            // unfold
+            UnFold();
+        }
+
+        if (!shouldBeFolded) {
+            fade = Mathf.Min(fade + fadeRate*Time.deltaTime, 1f);
+        }
+        shouldBeFolded = false;
+    }
+
+    void Fold(){
+        normalObject.SetActive(false);
+        if (foldedObject != null){
+            foldedObject.SetActive(true);
+        }
+    }
+
+    void UnFold(){
+        normalObject.SetActive(true);
+        if(foldedObject != null){
+            foldedObject.SetActive(false);
+        }
+    }
+
+    void CopyColiderFromChild(){
+        MeshCollider wallCollider = gameObject.AddComponent<MeshCollider>();
+        MeshCollider fullSizeCollider = normalObject.GetComponent<MeshCollider>();
+        wallCollider.sharedMesh = fullSizeCollider.sharedMesh;
+    }
+}
+
---
+++ Assets/Scripts/Player/ClearSight2.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearSight2 : MonoBehaviour
+{
+
+    public float transparencyRadius = 5f;
+    public GameObject player;
+    public LayerMask layerToFade;
+
+    private float distanceToPlayer;
+    private float castDistance;
+
+    void Start()
+    {
+        // set distance to player. Our camera has fixed distance to player
+        distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+        // try to estimate capsule cast distance, so it doesn't change object
+        // behind the player
+        castDistance = distanceToPlayer - transparencyRadius - 1f;
+    }
+
+    void Update()
+    {
+        RaycastHit[] hits; // you can also use CapsuleCastAll() 
+        // TODO: setup your layermask it improve performance and filter your hits. 
+        Vector3 direction = Vector3.Normalize(player.transform.position - transform.position);
+
+        //hits = Physics.RaycastAll(transform.position, direction, distanceToPlayer);
+        hits = Physics.CapsuleCastAll(transform.position, transform.position, transparencyRadius, direction, distanceToPlayer-transparencyRadius, layerToFade);
+
+        foreach (RaycastHit hit in hits)
+        {
+            AutoFold af = hit.collider.GetComponent<AutoFold>();
+            if (af == null)
+                continue;
+            // no autofold attached? go to next hit 
+            // TODO: maybe implement here a check for GOs that should not be affected like the player
+            af.BeFolded(); // get called every frame to reset the falloff
+        }
+    }
+}
+
