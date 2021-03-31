bd90028d56c996725d7082b5f05dff3194c62eb6
Milan Lajtoš <milan.lajtos@me.com>
2021-03-31 12:42:26 +0200

    Skrývajúce sa steny cez animáciu
    pls poriadne pretestovať

--- Assets/Scripts/Player/AutoFold.cs
+++ Assets/Scripts/Player/AutoFold.cs
@@ -1,67 +1,47 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class AutoFold : MonoBehaviour {
 
-    public float fadeRate = 0.5f;
-
     private GameObject normalObject;
     private GameObject foldedObject;
-    private float fade;
     private bool shouldBeFolded = false;
+    private float timer = 0.0f;
+    public AnimationCurve easing = AnimationCurve.EaseInOut(0, 0, 1, 1);
 
     public void BeFolded() {
-        Fold();
         shouldBeFolded = true;
-        fade = 0f;
     }
 
     private void Start() {
         normalObject = transform.Find("FullSize").gameObject;
+
+        // Tento celý blok môže ísť preč, viď koment vnútri
         Transform foldedTransform = transform.Find("CutSize");
         if(foldedTransform != null){
             foldedObject = foldedTransform.gameObject;
             Debug.Log("found folded version");
-        }
 
-        CopyColiderFromChild();
-
-        // unfold by default
-        UnFold();
-    }
-
-    // Update is called once per frame
-    private void Update() {
-        if (fade >= 1 && !shouldBeFolded){
-            // unfold
-            UnFold();
-        }
-
-        if (!shouldBeFolded) {
-            fade = Mathf.Min(fade + fadeRate*Time.deltaTime, 1f);
-        }
-        shouldBeFolded = false;
-    }
-
-    void Fold(){
-        normalObject.SetActive(false);
-        if (foldedObject != null){
-            foldedObject.SetActive(true);
-        }
-    }
-
-    void UnFold(){
-        normalObject.SetActive(true);
-        if(foldedObject != null){
+            // FoldedObject sa ani nepoužíva, môžeme úplne vypnúť
             foldedObject.SetActive(false);
         }
+
+        // prehodenie kolajdru z normalObject na tento objekt a vypnutie pôvodného
+        // ak sa nevypne pôvodný, tak hráča vedia dvere vystreliť do preč a ostane len čierna obrazovka
+        gameObject.AddComponent<MeshFilter>().mesh = normalObject.GetComponent<MeshFilter>().sharedMesh;
+        gameObject.AddComponent<MeshCollider>().convex = true;
+        normalObject.GetComponent<MeshCollider>().enabled = false;
     }
 
-    void CopyColiderFromChild(){
-        MeshCollider wallCollider = gameObject.AddComponent<MeshCollider>();
-        MeshCollider fullSizeCollider = normalObject.GetComponent<MeshCollider>();
-        wallCollider.sharedMesh = fullSizeCollider.sharedMesh;
+    // easing krivka má najmenšiu hodnotu okolo 0.2, t.j. celý objekt sa zmenší na 20% veľkosti
+    // týmto sa simuluje CutSize objekt
+    // keď je 0.0, tak preblikávajú nejaké svetlá zo spodu
+    // niektoré objekty v scéne nemáš ako prefaby (tie veľké čierne plochy), tak som im musel ručne skopírovať hodnotu krivky
+    private void Update() {
+        timer += Time.deltaTime * (shouldBeFolded ? -1 : +1);
+        timer = Mathf.Clamp(timer, 0, 1);
+        normalObject.transform.localScale = new Vector3(1, easing.Evaluate(timer), 1);
+
+        shouldBeFolded = false;
     }
 }
 
